98d08dd2c2c9410343fb809d8b93a35342db07c2
Kaili Nishihira and Robert Murray <kaili@>
2017-08-29 14:51:58 -0700

    Add more changes to Index and add more routes.

--- TravelForum/Controllers/HomeController.cs
+++ TravelForum/Controllers/HomeController.cs
@@ -15,16 +15,97 @@ namespace TravelForum.Controllers
       List<Region> allRegions = Region.GetAll();
       List<Country> allCountries = Country.GetAll();
       List<City> allCities = City.GetAll();
+      List<Tag> allTags = Tag.GetAll();
 
       Dictionary<string, object> model = new Dictionary<string, object> {};
       model.Add("posts", allPosts);
       model.Add("regions", allRegions);
       model.Add("countries", allCountries);
       model.Add("cities", allCities);
+      model.Add("tags", allTags);
 
       return View(model);
     }
 
+    [HttpPost("/posts/by-region")]
+    public ActionResult RegionPosts()
+    {
+      Region thisRegion = Region.Find(Int32.Parse(Request.Form["region-id"]));
+      List<Post> regionPosts = thisRegion.GetPosts();
+      List<Region> allRegions = Region.GetAll();
+      List<Country> allCountries = Country.GetAll();
+      List<City> allCities = City.GetAll();
+      List<Tag> allTags = Tag.GetAll();
+
+      Dictionary<string, object> model = new Dictionary<string, object> {};
+      model.Add("posts", regionPosts);
+      model.Add("regions", allRegions);
+      model.Add("countries", allCountries);
+      model.Add("cities", allCities);
+      model.Add("tags", allTags);
+
+      return View("Index", model);
+    }
+
+    [HttpPost("/posts/by-country")]
+    public ActionResult CountryPosts()
+    {
+      Country thisCountry = Country.Find(Int32.Parse(Request.Form["country-id"]));
+      List<Post> countryPosts = thisCountry.GetPosts();
+      List<Region> allRegions = Region.GetAll();
+      List<Country> allCountries = Country.GetAll();
+      List<City> allCities = City.GetAll();
+      List<Tag> allTags = Tag.GetAll();
+
+      Dictionary<string, object> model = new Dictionary<string, object> {};
+      model.Add("posts", countryPosts);
+      model.Add("regions", allRegions);
+      model.Add("countries", allCountries);
+      model.Add("cities", allCities);
+      model.Add("tags", allTags);
+
+      return View("Index", model);
+    }
+
+    [HttpPost("/posts/by-city")]
+    public ActionResult CityPosts()
+    {
+      City thisCity = City.Find(Int32.Parse(Request.Form["city-id"]));
+      List<Post> cityPosts = thisCity.GetPosts();
+      List<Region> allRegions = Region.GetAll();
+      List<Country> allCountries = Country.GetAll();
+      List<City> allCities = City.GetAll();
+      List<Tag> allTags = Tag.GetAll();
+
+      Dictionary<string, object> model = new Dictionary<string, object> {};
+      model.Add("posts", cityPosts);
+      model.Add("regions", allRegions);
+      model.Add("countries", allCountries);
+      model.Add("cities", allCities);
+      model.Add("tags", allTags);
+
+      return View("Index", model);
+    }
+
+    [HttpPost("/posts/by-tag")]
+    public ActionResult TagPosts()
+    {
+      Tag thisTag = Tag.Find(Int32.Parse(Request.Form["tag-id"]));
+      List<Post> tagPosts = thisTag.GetPosts();
+      List<Region> allRegions = Region.GetAll();
+      List<Country> allCountries = Country.GetAll();
+      List<City> allCities = City.GetAll();
+      List<Tag> allTags = Tag.GetAll();
+
+      Dictionary<string, object> model = new Dictionary<string, object> {};
+      model.Add("posts", tagPosts);
+      model.Add("regions", allRegions);
+      model.Add("countries", allCountries);
+      model.Add("cities", allCities);
+      model.Add("tags", allTags);
+
+      return View("Index", model);
+    }
 
 
   }
