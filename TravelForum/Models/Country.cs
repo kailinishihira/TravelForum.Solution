099a73800cdf343f7522eb1d5e3044ffb0898e1c
sonofakel <charlie.m.kelson@gmail.com>
2017-08-29 11:28:50 -0700

    resolved merge conflict.

--- TravelForum/Models/Country.cs
+++ TravelForum/Models/Country.cs
@@ -10,11 +10,7 @@ namespace TravelForum.Models
     private string _name;
     private int _regionId;
 
-<<<<<<< HEAD
     public Country(string name, int regionId=0, int id=0)
-=======
-    public Country(string name, int regionId, int id = 0)
->>>>>>> reply
     {
       _id  = id;
       _name = name;
