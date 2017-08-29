770bda32dd9f1116e585ef885c51391ce53f0c9b
Michel Woldemedihin <mwoldemedihin@gmail.com>
2017-08-28 17:10:28 -0700

    modified the city and country class

--- TravelForum/Models/City.cs
+++ TravelForum/Models/City.cs
@@ -8,11 +8,13 @@ namespace TravelForum.Models
   {
     private int _id;
     private string _name;
+    private int _countryId;
 
-    public City(string name, int id=0)
+    public City(string name, int countryId, int id=0)
     {
       _id= id;
       _name = name;
+      _countryId = countryId;
     }
   }
 }
--- TravelForum/Models/Country.cs
+++ TravelForum/Models/Country.cs
@@ -8,11 +8,13 @@ namespace TravelForum.Models
   {
     private int _id;
     private string _name;
+    private int _regionId;
 
     public Country(string name, int id=0)
     {
       _id= id;
       _name = name;
+      _regionId;
     }
   }
 }
