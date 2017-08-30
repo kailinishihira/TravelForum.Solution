703b05f784ea0e1f3b6332df6cdf049a87a9ae39
Guest User <Guest@epicodus-39.local>
2017-08-30 09:35:56 -0700

    Prepopulate database.

--- travel_forum.sql
+++ travel_forum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Aug 29, 2017 at 05:30 PM
+-- Generation Time: Aug 30, 2017 at 06:19 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.0.15
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `travel_forum`
 --
-CREATE DATABASE IF NOT EXISTS `travel_forum` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `travel_forum`;
 
 -- --------------------------------------------------------
 
@@ -34,6 +32,43 @@ CREATE TABLE `cities` (
   `country_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `cities`
+--
+
+INSERT INTO `cities` (`id`, `name`, `country_id`) VALUES
+(1, 'Seattle', 1),
+(2, 'New York', 1),
+(3, 'Brasilia', 2),
+(4, 'Rio Sao Paulo', 2),
+(5, 'Mexico City', 4),
+(6, 'Valencia', 5),
+(7, 'Kazan', 6),
+(8, 'Krakow', 7),
+(9, 'Plzen', 8),
+(10, 'Zadar', 9),
+(11, 'Stockholm', 10),
+(12, 'Akranes', 11),
+(13, 'Belfast', 12),
+(14, 'London', 13),
+(15, 'Barcelona', 14),
+(16, 'Lisbon', 15),
+(17, 'Venice', 16),
+(18, 'Marsala', 17),
+(19, 'Seoul', 18),
+(20, 'Hong Kong', 19),
+(21, 'Tokyo', 20),
+(22, 'Ho Chi Minh City', 20),
+(23, 'Bangkok', 22),
+(24, 'Oran', 23),
+(25, 'Cairo', 24),
+(26, 'Lagos', 25),
+(27, 'Tamale', 26),
+(28, 'Walvis Bay', 27),
+(29, 'Johannesburg', 28),
+(30, 'Juba', 29),
+(31, 'Antananarivo', 30);
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +80,41 @@ CREATE TABLE `countries` (
   `name` varchar(255) DEFAULT NULL,
   `region_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `countries`
+--
+
+INSERT INTO `countries` (`id`, `name`, `region_id`) VALUES
+(1, 'USA', 1),
+(3, 'Brazil', 2),
+(4, 'Mexico', 2),
+(5, 'Venezuela', 2),
+(6, 'Russia', 3),
+(7, 'Poland', 3),
+(8, 'Czech Republic', 3),
+(9, 'Croatia', 3),
+(10, 'Sweden', 4),
+(11, 'Iceland', 4),
+(12, 'Ireland', 4),
+(13, 'United Kingdom', 4),
+(14, 'Spain', 5),
+(15, 'Portugal', 5),
+(16, 'Italy', 6),
+(17, 'Sicily', 6),
+(18, 'South Korea', 7),
+(19, 'Hong Kong', 7),
+(20, 'Japan', 7),
+(21, 'Vietnam', 8),
+(22, 'Thailand', 8),
+(23, 'Algeria', 9),
+(24, 'Egypt', 9),
+(25, 'Nigeria', 10),
+(26, 'Ghana', 10),
+(27, 'Namibia', 11),
+(28, 'South Africa', 11),
+(29, 'South Sudan', 12),
+(30, 'Madagascar', 12);
 
 -- --------------------------------------------------------
 
@@ -63,6 +133,13 @@ CREATE TABLE `posts` (
   `country_id` int(11) NOT NULL DEFAULT '0',
   `region_id` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `name`, `start_date`, `end_date`, `text`, `city_id`, `country_id`, `region_id`) VALUES
+(1, '5 Must Do\'s in Seattle', 'John', '2017-08-18 00:00:00', '2017-08-11 00:00:00', '      fgdfdsfdsfds', 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -86,6 +163,24 @@ CREATE TABLE `regions` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `regions`
+--
+
+INSERT INTO `regions` (`id`, `name`) VALUES
+(1, 'North America'),
+(2, 'South America'),
+(3, 'Eastern Europe'),
+(4, 'Northern Europe'),
+(5, 'Western Europe'),
+(6, 'Southern Europe'),
+(7, 'East Asia'),
+(8, 'Southeast Asia'),
+(9, 'Northern Africa'),
+(10, 'Western Africa'),
+(11, 'Southern Africa'),
+(12, 'Eastern Africa');
 
 -- --------------------------------------------------------
 
@@ -172,17 +267,17 @@ ALTER TABLE `tags`
 -- AUTO_INCREMENT for table `cities`
 --
 ALTER TABLE `cities`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 --
 -- AUTO_INCREMENT for table `countries`
 --
 ALTER TABLE `countries`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `posts_tags`
 --
@@ -192,7 +287,7 @@ ALTER TABLE `posts_tags`
 -- AUTO_INCREMENT for table `regions`
 --
 ALTER TABLE `regions`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `replies`
 --
